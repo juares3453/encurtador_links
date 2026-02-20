5c03833019e78c5115c31883dc277511441bb787
juares3453 <juarescanalle@gmail.com>
2026-02-20 17:34:11 +0000

    Remove seção de links encurtados da visualização

--- resources/views/shortener.blade.php
+++ resources/views/shortener.blade.php
@@ -51,43 +51,6 @@
     <div class="alert alert-success mt-3">{{ session('success') }}</div>
 @endif
 
-@if(isset($shortUrls) && $shortUrls->count())
-    <div class="card mt-4">
-        <div class="card-header">Links Encurtados</div>
-        <div class="table-responsive">
-            <table class="table table-striped mb-0">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>URL Original</th>
-                        <th>Encurtado</th>
-                        <th>Acessos</th>
-                        <th>Criado em</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($shortUrls as $url)
-                    <tr>
-                        <td>{{ $url->id }}</td>
-                        <td><a href="{{ $url->original_url }}" target="_blank">{{ $url->original_url }}</a></td>
-                        <td><a href="{{ url($url->short_code) }}" target="_blank">{{ url($url->short_code) }}</a></td>
-                        <td>{{ $url->access_count }}</td>
-                        <td>{{ $url->created_at->format('d/m/Y H:i') }}</td>
-                        <td>
-                            <form action="{{ route('short-url.destroy', $url->id) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja excluir este link?');">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm">Excluir</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-@endif
 
 </div>
 </body>
