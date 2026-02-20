cada790f8a39962b1cb52b3efba85a1698687139
juares3453 <juarescanalle@gmail.com>
2026-02-20 17:27:16 +0000

    Reordena as rotas para links encurtados no arquivo web.php

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ use App\Http\Controllers\ShortUrlController;
 Route::get('/', [ShortUrlController::class, 'index'])->name('home');
 Route::delete('/short-url/{id}', [ShortUrlController::class, 'destroy'])->name('short-url.destroy');
 Route::post('/encurtar', [ShortUrlController::class, 'store'])->name('encurtar');
-Route::get('/{short_code}', [ShortUrlController::class, 'redirect'])->name('redirect');
 Route::get('/links', [ShortUrlController::class, 'listagem'])->name('links.listagem');
+Route::get('/{short_code}', [ShortUrlController::class, 'redirect'])->name('redirect');
