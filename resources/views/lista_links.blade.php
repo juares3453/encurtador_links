acd1a5a7955fa7f23a7563aa4aa346a0e97283b2
juares3453 <juarescanalle@gmail.com>
2026-02-23 12:55:56 +0000

    altera a classe do container para 'container-fluid' nas views de lista e encurtador de links

--- resources/views/lista_links.blade.php
+++ resources/views/lista_links.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-<div class="container py-5">
+<div class="container-fluid py-5">
     <div class="mb-4 d-flex justify-content-end gap-2">
         <a href="{{ route('home') }}" class="btn btn-outline-secondary">Encurtar novo link</a>
         @auth
