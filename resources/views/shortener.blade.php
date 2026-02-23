b69af617b0bbafc1e3ba50de47b2e46f9ae1e8e6
juares3453 <juarescanalle@gmail.com>
2026-02-23 12:59:50 +0000

    altera a classe do container para 'container' na view do encurtador de links

--- resources/views/shortener.blade.php
+++ resources/views/shortener.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-<div class="container-fluid py-5">
+<div class="container py-5">
         <div class="mb-4 d-flex justify-content-end gap-2">
             <a href="{{ route('links.listagem') }}" class="btn btn-outline-secondary">Ver todos os links</a>
             @auth
